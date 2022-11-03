962b08106692c18c18cc5367a84bef7a46201e28
syahrialalzaidan <16521445@mahasiswa.itb.ac.id>
2022-11-03 11:22:25 +0700

    nambahin rng.h

---
+++ game/rng.h
@@ -0,0 +1,12 @@
+#ifndef RNG_H
+#define RNG_H
+
+
+int rng();
+/* Fungsi RNG bakal ngereturn score yang diperoleh pemain
+ * Jumlah maksimal tebakan adalah 5 kali
+ * Fullscore adalah 10 dan akan berkurang 20% seiring dengan berkurangnya guess hingga benar
+*/
+
+
+#endif
