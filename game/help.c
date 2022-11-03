c8feabd479d44bace2639bfef031e4e96e8cdf69
syahrialalzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2022-11-03 11:18:58 +0700

    Create help.c

---
+++ game/help.c
@@ -0,0 +1,14 @@
+void HELP (){
+    printf("====================Need Help?====================\n");
+    printf("1. START -- Untuk menjalankan BNMO\n");
+    printf("2. LOAD -- Untuk membaca save file yang berisi list game yang dapat dimainkan, histori dan scoreboard game\n");
+    printf("3. SAVE -- Untuk menyimpan state game pemain saat ini\n");
+    printf("4. CREATEGAME -- Untuk menambahkan game baru pada daftar game\n");
+    printf("5. LISTGAME -- Untuk menampilkan daftar game yang disediakan oleh sistem\n");
+    printf("6. DELETEGAME -- Untuk menghapus sebuah game dari daftar game\n");
+    printf("7. QUEUEGAME -- Untuk mendaftarkan permainan kedalam list\n");
+    printf("8. PLAYGAME -- Untuk memainkan sebuah permainan\n");
+    printf("9. SKIPGAME -- Untuk melewatkan permainan sebanyak yang kamu mau\n");
+    printf("10. QUIT -- Untuk keluar dari program\n");
+    printf("11. HELP -- Untuk bantuin kamu\n");
+}   
