8e8c9394664bce01a4ffee1ff47c8a8b28c4b86d
devinaarr <Devinaarraudah@gmail.com>
2022-11-03 23:48:45 +0700

    TEST AJLAJH
    LAGI NYOBA PUSHHHH

---
+++ hello/hello.c
@@ -0,0 +1,12 @@
+/*NIM   : 18221113
+Nama    : Devina Ar'raudah
+Tanggal : 4 September 2022
+Topik praktikum : Pengenalan Bahasa C
+Deskripsi   : Program dalam Bahasa C dengan nama hello.c yang menuliskan Hello, World! ke layar*/
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  return 0;
+}
