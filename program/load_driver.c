889f210b36307b8a51e13bc1df039b1edfcc2c5c
Attariq Muhammad Azhar <attariq03.azhar@gmail.com>
2022-11-07 02:39:38 +0700

    updated load.c and added new ADT version of mesinkar

---
+++ program/load_driver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ADT/arraydin/arraydin.h"
+#include "ADT/mesinkarkata/mesinkarfile.h"
+#include "ADT/mesinkarkata/mesinkatafile.h"
+#include "load.h"
+
+int main() {
+    ArrayDin Games = MakeArrayDin();
+    load("config.txt", &Games);
+}
