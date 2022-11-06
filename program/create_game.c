205654d2f54a1c6a3b56113b08def33c939429f7
arifu11 <arifuddinachmad11@gmail.com>
2022-11-06 20:48:54 +0700

    benerin list ama create

--- program/create_game.c
+++ program/create_game.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include "arraydin.h"
+#include "mesinkata.h"
+#include "mesinkar.h"
 
-void creategame (ArrayDin *array, char* string){
-    InsertLast(array, string);
+void creategame (ArrayDin *array){
+/* I.S. sembarang */
+/* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1 */
+
+    char* string;
+    printf("Masukkan nama game yang akan ditambahkan: ");
+    STARTWORD();
+    string = currentWord.TabWord;
+    printf("Game berhasil ditambahkan\n");
+    
+
+
+    //printf("Tes %s", string);
+
+    //printf("%d", currentWord.Length);
+    InsertLast(&array, *string);
+    printf("Tes %s", (*array).A[(*array).Neff]);
+}
+
+int main()
+{
+    ArrayDin S;
+    S = MakeArrayDin();
+    creategame(&S);
 }
