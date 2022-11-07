2d6b4daa1ca968012cd7b0a966350b7f3cde7372
syahrialalzaidan <16521445@mahasiswa.itb.ac.id>
2022-11-07 07:08:30 +0700

    raphin

--- game/dinnerdash.c
+++ game/dinnerdash.c
@@ -1,7 +1,6 @@
 #include "dinnerdash.h"
 
-void generatemenu(Queue *q)
-{
+void generatemenu(Queue *q) {
     ElType food;
     food.durasi = (rand() % 5) + 1;
     food.ketahanan = (rand() % 5) + 1;
@@ -11,56 +10,40 @@ void generatemenu(Queue *q)
     enqueue(q, food);
 }
 
-boolean isCook(Word kata)
-{
-    boolean output;
-    if (kata.Length == 4)
-    {
-        if (kata.TabWord[0] == 'C')
-        {
-            if (kata.TabWord[1] == 'O')
-            {
-                if (kata.TabWord[2] == 'O')
-                {
-                    if (kata.TabWord[3] == 'K') output = true;
-                    
+boolean isCook(Word kata) {
+    boolean output = false;
+    if (kata.Length == 4) {
+        if (kata.TabWord[0] == 'C') {
+            if (kata.TabWord[1] == 'O') {
+                if (kata.TabWord[2] == 'O') {
+                    if (kata.TabWord[3] == 'K') output = true; 
                 }
             }
         }
     }
-    else output = false;
     return output;
 }
 
-boolean isCommandValid(Word kata)
-{
+boolean isCommandValid(Word kata) {
     boolean output = false;
-    if (kata.Length == 5)
-    {
-        if (kata.TabWord[0] == 'S')
-        {
-            if (kata.TabWord[1] == 'E')
-            {
-                if (kata.TabWord[2] == 'R')
-                {
-                    if (kata.TabWord[3] == 'V')
-                    {
+    if (kata.Length == 5) {
+        if (kata.TabWord[0] == 'S') {
+            if (kata.TabWord[1] == 'E') {
+                if (kata.TabWord[2] == 'R'){
+                    if (kata.TabWord[3] == 'V') {
                         if (kata.TabWord[4] == 'E') output = true;
-                        
                     }
                 }
             }
         }
     }
-    else
-    {
+    else {
         if (isCook(kata)) output = true;
     }
     return output;
 }
 
-boolean isdigitvalid(Word kata)
-{
+boolean isdigitvalid(Word kata) {
     boolean output = true;
     for (int i = 1; i < kata.Length; i++)
     {
@@ -69,77 +52,62 @@ boolean isdigitvalid(Word kata)
     return output;
 }
 
-int getID(Word kata)
-{
+int getID(Word kata) {
     int output = 0;
-    if (kata.Length == 2)
-    {
+    if (kata.Length == 2) {
         return kata.TabWord[1] - '0';
     }
-    else if (kata.Length == 3)
-    {
+    else if (kata.Length == 3) {
         return (kata.TabWord[1] - '0') * 10 + (kata.TabWord[2] - '0');
     }
 }
 
-void serve(Queue *q1, Queue *q2, int i, int *saldo, int *count, boolean *sukses)
-{ // q1 = menu; q2 = cook; i = id
+void serve(Queue *q1, Queue *q2, int i, int *saldo, int *count, boolean *sukses) { // q1 = menu; q2 = cook; i = id
     boolean done = false, found = false;
     ElType food;
     int x = 0;
-    while (x < length(*q2) && !found)
-    {
-        if ((*q2).buffer[x].id == i)
-        {
+    while (x < length(*q2) && !found) {
+        if ((*q2).buffer[x].id == i) {
             found = true;
             if ((*q2).buffer[x].durasi <= 0) done = true;
             
         }
         else x++;
     }
-    if (!found)
-    {
+    if (!found) {
         printf("Masakan belum dimasak\n");
         *sukses = false;
     }
-    else
-    {
-        if (done)
-        {
-            if (i == IDX_HEAD(*q1))
-            {
+    else {
+        if (done) {
+            if (i == IDX_HEAD(*q1)) {
                 printf("Berhasil mengantar M%d\n", i);
                 *count++;
                 *saldo += (HEAD(*q1).harga);
                 dequeue(q1, &food);
                 boolean found = false;
                 int x = IDX_HEAD(*q2);
-                while (x < length(*q2) && !found)
-                {
-                    if ((*q2).buffer[x].id == i)
-                    {
+                while (x < length(*q2) && !found) {
+                    if ((*q2).buffer[x].id == i) {
                         found = true;
                         (*q2).buffer[x].ketahanan = 0;
                     }
                     else x++;  
                 }
             }
-            else
-            {
+            else {
                 printf("M%d belum dapat disajikan karena M%d belum selesai\n", i, IDX_HEAD(*q1));
                 *sukses = false;
             }
         }
-        else
-        {
+        else {
             printf("M%d belum selesai dimasak\n", i);
             *sukses = false;
         }
     }
 }
 
-void playdinnerdash()
-{
+void playdinnerdash() {
     printf("Selamat Datang di Diner Dash!\n");
     printf("\n");
     int saldo = 0, countserve = 0;
@@ -148,8 +116,7 @@ void playdinnerdash()
     // generate menu
     Queue menu;
     CreateQueue(&menu);
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         generatemenu(&menu);
     }
     Queue cook;
@@ -157,49 +124,36 @@ void playdinnerdash()
     boolean conds = false;
     
 
-    while (length(menu) <= 7 && countserve <= 15)
-    {
+    while (length(menu) <= 7 && countserve <= 15) {
         // ngelayananin input cook/serve
-        if (length(menu) <= 7)
-        {
-            printf("============================\n\n");
-            printf("Saldo: %d\n\n", saldo);
-            daftarmenu(menu);
-            daftarcook(cook);
-            daftarserve(cook);
-            printf("MASUKKAN COMMAND: ");
-            STARTWORD();
-        }
+        printf("============================\n\n");
+        printf("Saldo: %d\n\n", saldo);
+        daftarmenu(menu);
+        daftarcook(cook);
+        daftarserve(cook);
+        printf("MASUKKAN COMMAND: ");
+        STARTWORD();
+    
         if (conds) generatemenu(&menu);
-            
-        if (isCommandValid(currentWord))
-        {
+        if (isCommandValid(currentWord)) {
             boolean masak = isCook(currentWord);
             conds = true;
             ADVWORD();
-            if (isdigitvalid(currentWord) && currentWord.TabWord[0] == 'M' && currentWord.Length > 1 && currentWord.Length <=3 )
-            {
+            if (isdigitvalid(currentWord) && currentWord.TabWord[0] == 'M' && currentWord.Length > 1 && currentWord.Length <=3 ) {
                 int foodid = getID(currentWord);
-                if (masak)
-                {
+                if (masak) {
                     int i = 0;
                     boolean found = false;
-                    if (isEmpty(cook))
-                    {
+                    if (isEmpty(cook)) {
                         printf("Berhasil memasak M%d\n", foodid);
                         enqueue(&cook, menu.buffer[foodid]);
                     }
-                    else
-                    {
-                        while (i <= IDX_TAIL(cook) && (!found))
-                        {
-                            if (cook.buffer[i].id == foodid) found = true;
-                            
+                    else {
+                        while (i <= IDX_TAIL(cook) && (!found)) {
+                            if (cook.buffer[i].id == foodid) found = true; 
                             else i++;
-                            
                         }
-                        if (!found && length(menu) <= 7)
-                        {
+                        if (!found && length(menu) <= 7) {
                             printf("Berhasil memasak M%d\n", foodid);
                             enqueue(&cook, menu.buffer[foodid]);
                         }
