5155a6288e54026b9ab0933032429939bcef978a
iskandarmrp <iskandarmudarp@gmail.com>
2022-11-08 01:02:01 +0700

    play queue skip

--- program/playgame.c
+++ program/playgame.c
@@ -1,34 +1,85 @@
+#include "playgame.h"
+/*
 #include <stdio.h>
-#include "queue.h"
+#include "queue.c"
+#include "boolean.h"
+#include "mesinkata.c"
+#include "mesinkar.c"
+#include "arraydin.c"
+#include <stdlib.h>
+#include "queuegame.c"
+#include "playgame.h"
+*/
 
-void playgame(Queue *queuegame){
+void playgame(Queue *qg, ArrayDin array){
     printf("Berikut adalah daftar game-mu\n");
-    if (isEmpty(*queuegame)){
-        queuegame();
+    if (!isEmpty(*qg)){
+        listqueue(qg);
+    }
+
+    if (isEmpty(*qg)){
+        queuegame(qg, array);
     } else{
-        char *rng = "RNG";
-        char *dinnerdash = "Dinner Dash";
-        char *... = "...";
+        char* rng = "RNG";
+        char* dinnerdash = "DINNER DASH";
+        char* tictactoe = "TIC TAC TOE";
         int i;
-        for (i = (*queuegame).idxHead; i < (*queuegame).idxTail + 1; i++){
-            printf("%s\n", (*queuegame).buffer[i]);
-        }
         printf("\n");
-        if ((*queuegame).buffer[(*queuegame).idxHead] == "RNG"){
+
+        if ((*qg).buffer[(*qg).idxHead] == rng){
             printf("Loading RNG ...");
-            playrng;
-        } else if((*queuegame).buffer[(*queuegame).idxHead] == "Dinner Dash"){
+            /*
+            rng();
+            */
+            printf("rng()");
+        } else if((*qg).buffer[(*qg).idxHead] == dinnerdash){
             printf("Loading Dinner Dash ...");
-            playdinnerdash;
-        } else if((*queuegame).buffer[(*queuegame).idxHead] == "..."){
-            printf("Loading ... ...");
-            play ...;
+            /* 
+            srand(time(0));
+            playdinnerdash();
+            */
+            printf("dinner dash");
+        } else if((*qg).buffer[(*qg).idxHead] == tictactoe){
+            printf("Loading Tic-Tac-Toe");
+            /*
+            playtictactoe;
+            */
+            printf("tictactoe");
         } else{
-            printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", (*queuegame).buffer[(*queuegame).idxHead]);
+            printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", (*qg).buffer[(*qg).idxHead]);
             printf("Silahkan pilih game lain.\n");
         }
         printf("\n");
+
         ElType a;
-        dequeue(queuegame, &a);
+        dequeue(qg, &a);
     }
 }
+
+/*
+int main(){
+    Queue q;
+    CreateQueue(&q);
+    ArrayDin a = MakeArrayDin();
+    ElType as, bc, cc;
+    as = "a";
+    bc = "b";
+    cc = "c";
+    InsertFirst(&a, as);
+    InsertLast(&a, bc);
+    InsertLast(&a, cc);
+
+    ElType y, b, c;
+    
+    y = "RNG";
+    b = "DINNER DASH";
+    c = "LALALA";'
+
+    enqueue(&q, y);
+    enqueue(&q, b);
+
+    enqueue(&q, c);
+    playgame(&q, a);
+    return 0;
+}
+*/
--- program/queuegame.c
+++ program/queuegame.c
@@ -1,25 +1,72 @@
+#include "queuegame.h"
+
+/*
 #include <stdio.h>
-#include "queue.h"
-#include "arraydin.h"
+#include "queue.c"
+#include "boolean.h"
+#include "mesinkata.c"
+#include "mesinkar.c"
+#include "arraydin.c"
+#include <stdlib.h>
+#include "queuegame.h"
+*/
 
 void queuegame(Queue *queuegame, ArrayDin array){
-    int i, j, x;
-    printf("Berikut adalah daftar antrian game-mu\n")
+    int x;
+    printf("Berikut adalah daftar antrian game-mu\n");
     if (!isEmpty(*queuegame)){
-        for (i = (*queuegame).idxHead; i < (*queuegame).idxTail + 1; i++){
-            printf("%s\n", (*queuegame).buffer[i]);
-        }
+        listqueue(*queuegame);
     }
     printf("\n");
+
     listgame(array);
     printf("\n");
-    scanf("Nomor Game yang mau ditambahkan ke antrian : %d", &x);
-    /*gaboleh scanf, pake stdin, fscanf*/
+
+    int j;
+    printf("Game: \n");
+    for (j = 0; j < array.Neff; j++){
+        printf("%d. %s\n", j+1, array.A[j]);
+    }
+
+    printf("Nomor Game yang mau ditambahkan ke antrian: ");
+    STARTWORD();
+    x = currentWord.TabWord[0] - '0';
     printf("\n");
+
     if ((x > array.Neff) || (x < 1)){
-        printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list.\n")
+        printf("Nomor permainan tidak valid, silahkan masukkan nomor game pada list.\n");
     } else{
-        enqueue(*queuegame, array.A[x - 1]);
+        enqueue(queuegame, array.A[x - 1]);
         printf("Game berhasil ditambahkan kedalam daftar antrian.\n");
     }
 }
+
+void listqueue(Queue *queuegame){
+    int i;
+    for (i = (*queuegame).idxHead; i < (*queuegame).idxTail + 1; i++){
+        printf("%s\n", (*queuegame).buffer[i]);
+    }
+}
+
+
+
+
+/*
+int main(){
+    Queue q;
+    CreateQueue(&q);
+    ArrayDin a = MakeArrayDin();
+    ElType as, b, c;
+    as = "a";
+    b = "b";
+    c = "c";
+    InsertFirst(&a, as);
+    InsertLast(&a, b);
+    InsertLast(&a, c);
+
+    queuegame(&q, a);
+    listqueue(&q);
+    queuegame(&q, a);
+    listqueue(&q);
+    return 0;  
+}*/
