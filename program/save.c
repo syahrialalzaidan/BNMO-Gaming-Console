a5c75c9ecb2157a35af6a9b99b526629fcb3f33d
Attariq Muhammad Azhar <attariq03.azhar@gmail.com>
2022-11-08 11:13:48 +0700

    added save.c and its driver

---
+++ program/save.c
@@ -0,0 +1,20 @@
+#include "save.h"
+#include "load.h"
+
+void save(ArrayDin *Games, char* filename) {
+    FILE *file;
+    char* filepath = AddPath(filename); 
+    file = fopen(filepath, "w");
+    if (file != NULL) {
+        fprintf(file, "%d\n", Games->Neff);
+        //overwrite file per line with contents of games
+        for (int i = 0; i < Games->Neff; i++) {
+            if (i == Games->Neff - 1) {
+                fprintf(file, "%s", Games->A[i]);
+            } else {
+                fprintf(file, "%s\n", Games->A[i]);
+            }
+        }
+        fclose(file);
+    }
+}
