e0af6b43ea619264a773689e815a540f35f27ed7
iskandarmrp <iskandarmudarp@gmail.com>
2022-12-02 19:24:58 +0700

    hm

--- game/Hangman.h
+++ game/Hangman.h
@@ -11,8 +11,31 @@
 
 //void generatemap(MapChar *kamus);
 void loadkamus(char* filename, Set* kamus);
+/*  Membaca save file yang berisi list game yang dapat dimainkan.
+    I.S. : filename terdefinisi dan array games kosong.
+    F.S. : Array games terisi dengan list game yang ada pada save file. */
+
 char UpperHuruf(char input);
+/*Mengubah semua huruf kecil ke huruf besar*/
+
+char* UpperKata(char* input);
+/*Membuat input kata menjadi huruf besar semua*/
+
+void hangmanart();
+/*Animasi kekalahan HAHAHA*/
+
 int countkataHM(char *kata);
+/*Menghitung panjang kata*/
+
+void hangmanascii();
+/*Animasi hangman*/
+
 void playtebakkataHM(float* scoretotal, float* nyawa);
+/*Permainan utama*/
+
 boolean isstringequalHM(char *kata1, char *kata2);
+/*Fungsi untuk cek apakah dua string yang dimasukkan sama.
+Ini dimanfaatkan untuk memvalidasi masukkan pengguna apakah sesuai KJ*/
+
 void Hangman(float *skor, boolean *playtebakkataHM);
+//Fungsi utama yang dipanggil
