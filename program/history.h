887c1645a2d45ef499dc6250e38b712693feb205
devinaarr <Devinaarraudah@gmail.com>
2022-12-02 19:00:08 +0700

    nambahin dikit

--- program/history.h
+++ program/history.h
@@ -2,5 +2,9 @@
 #include "ADT/Stack/stack.h"
 
 void PrintHistory(Stack History, int n);
+/* Memberikan output n permainan terakhir yang telah dimainkan*/
 
 void ResetHistory(Stack *History);
+/* Me-reset history permainan
+    I.S.: History terisi 
+    F.S.: Setiap elemen di dalam stack history di-pop sampai history kosong. */
--- program/scoreboard.h
+++ program/scoreboard.h
@@ -5,11 +5,15 @@
 #include "ADT/mesinkarkata/mesinkata.h"
 
 int StringLength(char* string);
+/* Mengembalikan nilai panjang suatu string */
 
 int CountDigits(float n);
+/* Mengembalikan nilai panjang suatu float */
 
 void PrintScoreboard(Map M);
 /* Menampilkan Scoreboard yang ada ke layar. */
 
 void ResetScoreboard(ListMap *LM, ArrayDin G);
-
+/* Me-reset scoreboard
+    I.S.: Scoreboard terisi
+    F.S.: Scoreboard suatu/semua permainan di-reset, tergantung masukan user. */
